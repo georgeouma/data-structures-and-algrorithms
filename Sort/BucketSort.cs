6204615ed1582acd1cf275e8947f98f4b916a4ec
George Ouma <georgengura@gmail.com>
2021-01-30 14:21:40 +0300

    bucket sort implementation

--- Sort/BucketSort.cs
+++ Sort/BucketSort.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Sort
 {
-    public class BucketSort : ISort
+    public class BucketSort
     {
-        public T[] Sort<T>(T[] array) where T : IComparable
+        public int[] Sort(int[] array)
         {
-            throw new NotImplementedException();
+            List<int> sortedArray = new List<int>();
+
+            int numOfBuckets = 10;
+
+            //Create buckets
+            List<int>[] buckets = new List<int>[numOfBuckets];
+            for (int i = 0; i < numOfBuckets; i++)
+            {
+                buckets[i] = new List<int>();
+            }
+
+            //Iterate through the passed array 
+            //and add each integer to the appropriate bucket
+            for (int i = 0; i < array.Length; i++)
+            {
+                int bucket = (array[i] / numOfBuckets);
+                buckets[bucket].Add(array[i]);
+            }
+
+            //Sort each bucket and add it to the result List
+            for (int i = 0; i < numOfBuckets; i++)
+            {
+                List<int> temp = new InsertionSort().Sort(buckets[i].ToArray()).ToList<int>();
+                sortedArray.AddRange(temp);
+            }
+            return sortedArray.ToArray();
         }
     }
 }
