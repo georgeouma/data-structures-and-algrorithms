b6182f6cd25ed1aee5accca0211a60238b04d6f9
George Ouma <georgengura@gmail.com>
2021-01-30 13:20:52 +0300

    quick sort implementation

--- Sort/QuickSort.cs
+++ Sort/QuickSort.cs
@@ -6,21 +6,40 @@ namespace Sort
     {
         public T[] Sort<T>(T[] array) where T : IComparable
         {
-            throw new NotImplementedException();
+            Sort(array, 0, array.Length - 1);
+
+            return array;
         }
 
-        private T[] Sort<T>(T[] array, int start, int end) where T : IComparable
+        private void Sort<T>(T[] array, int start, int end) where T : IComparable
         {
-            if(start < end)
+            if (start < end)
             {
-
-            }
-            return array;
+                int pivotIndex = Partition(array, start, end);
+                Sort(array, start, pivotIndex - 1);
+                Sort(array, pivotIndex + 1, end);
+            }           
         }
 
         private int Partition <T>(T[] array, int start, int end) where T : IComparable
         {
-            throw new NotImplementedException();
+            T pivot = array[end];
+            int i = start;
+            for (int j = start; j <= end-1; j++)
+            {
+                if(array[j].CompareTo(pivot) < 0)
+                {
+                    T temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                }
+            }
+
+            array[end] = array[i];
+            array[i] = pivot;
+
+            return i;
 
         }
     }
