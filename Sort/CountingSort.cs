c916dba4834c1defcb81478333605417a7e54860
George Ouma <georgengura@gmail.com>
2021-01-30 14:12:04 +0300

    counting sort impleentation

--- Sort/CountingSort.cs
+++ Sort/CountingSort.cs
@@ -2,11 +2,29 @@
 
 namespace Sort
 {
-    public class CountingSort : ISort
+    public class CountingSort
     {
-        public T[] Sort<T>(T[] array) where T : IComparable
+        public int[] Sort(int[] array)
         {
-            throw new NotImplementedException();
-        }
+			int[] temp = new int[11];
+
+			for (int i = 0; i < array.Length; i++)
+			{ // populate the temp array
+				temp[array[i]] = temp[array[i]] + 1;
+			}
+
+			int curr = 0;
+
+			for (int i = 0; i < temp.Length; i++)
+			{ // k iterations
+				for (int j = 0; j < temp[i]; j++)
+				{ // n/k iterations for each (avg)
+					array[curr] = i;
+					curr++;
+				}
+			}
+
+			return array;
+		}
     }
 }
