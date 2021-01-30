2bc73f1fd30271981adfdfc8e2cafa98a0f8381d
George Ouma <georgengura@gmail.com>
2021-01-30 22:49:44 +0300

    modified test data

--- Sort.Tests/RadixSortTests.cs
+++ Sort.Tests/RadixSortTests.cs
@@ -8,7 +8,7 @@ namespace Sort.Tests
         [TestMethod()]
         public void SortTest()
         {
-            int[] array = new int[] { 5, 4, 3, 2, 1 };
+            int[] array = new int[] { 536, 484, 343, 287, 198 };
             var sortedArray = new RadixSort().Sort(array);
 
             Assert.AreEqual(sortedArray.Length, array.Length);
