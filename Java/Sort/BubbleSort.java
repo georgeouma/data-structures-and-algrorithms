a94807bb024f3864a3dc9f383cc2363233dd57c4
George Ouma <georgengura@gmail.com>
2021-01-31 18:08:09 +0300

    Bubble sort implementation java

---
+++ Java/Sort/BubbleSort.java
@@ -0,0 +1,21 @@
+package Java.Sort;
+
+/**
+ * BubbleSortort
+ */
+public class BubbleSort {
+
+    public <T extends Comparable<T>> T[] sort(T[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+}
