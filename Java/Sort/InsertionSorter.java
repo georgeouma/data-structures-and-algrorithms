2672badda1b34d7499a4d7fc3d5b94c6368c0f97
George Ouma <georgengura@gmail.com>
2021-01-31 16:05:55 +0300

    fix insertion sort

--- Java/Sort/InsertionSorter.java
+++ Java/Sort/InsertionSorter.java
@@ -2,35 +2,19 @@ package Java.Sort;
 
 public class InsertionSorter {
 
-	public void sort(DoublyLinkedList data) {
-		DoublyLinkedNode node = data.getHead();
-		while (node != null) { // This is the outer loop where node represents the node pointed by the black pointer in the video 
-			DoublyLinkedNode prevNode = node.getPreviousNode();
-			while (prevNode != null && (prevNode.getData() > node.getData())) {
-				prevNode = prevNode.getPreviousNode();
+	public <T extends Comparable<T>> T[] sort(T[] array) {
+		for (int i = 0; i < array.length; i++) {
+			T key = array[i];
+			int pointer = i;
+			while (pointer > 0 && key.compareTo(array[pointer - 1]) < 0) {
+				array[pointer] = array[pointer - 1];
+				pointer--;
 			}
-			// At this stage prevNode represents the node pointed by the green pointer (in video lecture)
-			DoublyLinkedNode next = node.getNextNode(); // we keep a reference to the next node of the black pointer for easier access
-			if (prevNode != null || !data.isHead(node)) {
-				node.getPreviousNode().setNextNode(next);
-				if (next != null) {
-					next.setPreviousNode(node.getPreviousNode());
-				}
-				node.setPreviousNode(prevNode);
-			}
-			if (prevNode == null) { // set the node as head
-				if (!data.isHead(node)) {
-					node.setNextNode(data.getHead());
-					node.getNextNode().setPreviousNode(node);
-					data.setHead(node);
-				}
-			} else { // of course the following connections can be made only when the prevNode is not null
-				node.setNextNode(prevNode.getNextNode());
-				prevNode.getNextNode().setPreviousNode(node);
-				prevNode.setNextNode(node);
-			}
-			node = next;
+
+			array[pointer] = key;
 		}
+
+		return array;
 	}
 
 }
